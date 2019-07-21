591fb44c28440a4906f2957a0fde7a5195da4bec
Beatrize <heybeatrize@gmail.com>
2019-07-20 18:28:25 -0700

    refactored 1

--- rpc.py
+++ rpc.py
@@ -6,18 +6,29 @@ print("..scissors")
 player1 = input("Player 1, make your move: ")
 player2 = input("Player 2, make your move: ")
 
-if player1 == "rock" and player2 == "scissors":
+#if player1 == "rock" and player2 == "scissors":
+#	print("player1 wins!")
+#elif player1 == "rock" and player2 == "paper":
+#	print("player2 wins!")
+#elif player1 == "paper" and player2 == "rock":
+#	print("player1 wins!")
+#elif player1 == "paper" and player2 == "scissors":
+#	print("player2 wins!")
+#elif player1 == "scissors" and player2 == "rock":
+#	print("player2 wins!")
+#elif player1 == "scissors" and player2 == "paper":
+#	print("player1 wins!")
+#elif player1 == player2:
+#	print("Its a tie!")
+#else:
+#	print("something went wrong")
+
+#refactored
+
+if (player1 == "rock" and player2 == "scissors") or (player1 == "paper" and player2 == "rock") or (player1 == "scissors" and player2 == "paper"):
 	print("player1 wins!")
-elif player1 == "rock" and player2 == "paper":
+elif (player1 == "rock" and player == "paper") or (player1 == "paper" and player2 == "scissors") or (player1 == "scissors" and player2 == "rock"):
 	print("player2 wins!")
-elif player1 == "paper" and player2 == "rock":
-	print("player1 wins!")
-elif player1 == "paper" and player2 == "scissors":
-	print("player2 wins!")
-elif player1 == "scissors" and player2 == "rock":
-	print("player2 wins!")
-elif player1 == "scissors" and player2 == "paper":
-	print("player1 wins!")
 elif player1 == player2:
 	print("Its a tie!")
 else:
