850721b07c2fb5001e94baba630b7c3086e83f6b
Beatrize <heybeatrize@gmail.com>
2019-07-20 20:22:42 -0700

    refactored for AI

--- rpc.py
+++ rpc.py
@@ -31,13 +31,13 @@
 # colt's more effiecient refactor 2 lol
 
 #somethings wrong in terminal
-print("..rock")
-print("..paper")
-print("..scissors")
+# print("..rock")
+# print("..paper")
+# print("..scissors")
 
-player1 = input("Player 1, make your move: ")
-print("****NO CHEATING\n\n" * 20)
-player2 = input("Player 2, make your move: ")
+#player1 = input("Player 1, make your move: ")
+#print("****NO CHEATING\n\n" * 20)
+#player2 = input("Player 2, make your move: ")
 
 #if player1 == player2:
 #	print("Its a tie!")
@@ -61,13 +61,48 @@ player2 = input("Player 2, make your move: ")
 
 #better shorter version
 
-if player1 == player2:
-	print("Draw")
-elif player1 == "rock" and player2 == "scissors":
-	print("player1 wins!")
-elif player1 == "paper" and player2 == "rock":
-	print("player1 wins!")
-elif player1 == "scissors" and player2 == "paper":
-	print("player1 wins!")
+#if player1 == player2:
+#	print("Draw")
+#elif player1 == "rock" and player2 == "scissors":
+#	print("player1 wins!")
+#elif player1 == "paper" and player2 == "rock":
+#	print("player1 wins!")
+#elif player1 == "scissors" and player2 == "paper":
+#	print("player1 wins!")
+#else:
+#	print("player2 wins!")
+
+# AI version
+import random
+
+rand_num = random.randint(0,2)
+if rand_num == 0:
+	computer = "rock"
+elif rand_num == 1:
+	computer = "paper"
 else:
-	print("player2 wins!")
+	computer = "scissors"
+print(computer)
+
+player = input("Player1, make your move: ")
+
+
+if player == computer:
+	print("Its a tie!")
+elif player == "rock":
+	if computer == "scissors":
+		print("player wins!")
+	elif computer == "paper":
+		print("computer wins!")
+elif player == "paper":
+	if computer == "rock":
+		print("player wins!")
+	elif computer == "scissors":
+		print("computer wins!")
+elif player == "scissors":
+	if computer == "paper":
+		print("player wins!")
+	elif computer == "rock":
+	 	print("computer wins!")
+else:
+	print("something went wrong")
